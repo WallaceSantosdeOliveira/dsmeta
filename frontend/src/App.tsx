96e7e65687e6f9f17b2ab76ab0c1b13fd168b29b
Wallace SantosdeOliveira <wallacesantos447@gmail.com>
2022-07-12 10:16:09 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
-import NotificationButton from "./components/NotificationButton";
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
+
 
 function App() {
   return (
     <>
-      <h1>Ola Mundo</h1>
-      <NotificationButton />
-      <NotificationButton />      
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container"></div>
+          <SalesCard />
+        </section>
+      </main>
     </>
   );
 }
 
-export default App
+export default App;
