f44340786853aa202d7f6075311de4b76ff92d11
Wallace SantosdeOliveira <wallacesantos447@gmail.com>
2022-07-16 22:22:25 -0300

    Update Linkedin

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -10,7 +10,7 @@ function Header() {
         <h1>DSMeta</h1>
         <p>
           Desenvolvido por
-          <a href="https://www.linkedin.com/in/wallace-santos-de-oliveira-25b99a170/">
+          <a href="https://www.linkedin.com/in/wallace-santos-de-oliveira-6a1863244/">
             @Wallace Santos
           </a>
         </p>
